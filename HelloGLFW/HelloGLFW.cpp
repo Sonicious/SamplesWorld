fd1c0cd47a6b79a7692f84cecf64ad914b6544d1
Martin Reinhardt <martin.reinhardt.87@gmail.com>
2019-08-20 17:07:06 +0200

    mac compatibility

--- HelloGLFW/HelloGLFW.cpp
+++ HelloGLFW/HelloGLFW.cpp
@@ -81,8 +81,9 @@ int main(void)
   }
   // Set Error Callback
   glfwSetErrorCallback(error_callback);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.2
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   // Create a windowed mode window and its OpenGL context
   GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
