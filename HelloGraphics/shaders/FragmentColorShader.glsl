c08eacba935339cad0c93f03619c3f52ac25fc47
Martin Reinhardt <martin.reinhardt.87@gmail.com>
2021-08-24 08:17:52 +0200

    WIP: Mac Specifics of OpenGL41

--- HelloGraphics/shaders/FragmentColorShader.glsl
+++ HelloGraphics/shaders/FragmentColorShader.glsl
@@ -1,12 +1,11 @@
 #version 410 core
 
-layout(location = 0) in vec4 fColor;
+layout(location = 0) in vec4 color;
 layout(location = 1) in vec2 texCoord;
 
 out vec4 outColor;
 
 void main()
 {
-  outColor = fColor;
-  //outColor = vec4(1.0f, 1.0f, 1.0f, 0.5f);
+  outColor = color;
 }
--- HelloGraphics/shaders/FragmentTextureShader.glsl
+++ HelloGraphics/shaders/FragmentTextureShader.glsl
@@ -1,6 +1,6 @@
 #version 410 core
 
-layout(location = 0) in vec4 fColor;
+layout(location = 0) in vec4 color;
 layout(location = 1) in vec2 texCoord;
 
 out vec4 outColor;
